bb637fd24ed3b52d7297dbf354479b79083ef5f2
leelavery <lee.lavery@intilery.com>
2015-08-24 10:54:44 +0100

    Add additional error handling

--- Sortable.js
+++ Sortable.js
@@ -542,7 +542,7 @@
 						rootEl.insertBefore(dragEl, cloneEl || nextEl);
 					}
 					else if (!canSort) {
-						rootEl.appendChild(dragEl);
+						try { rootEl.appendChild(dragEl); } catch(e) { }
 					}
 
 					return;
